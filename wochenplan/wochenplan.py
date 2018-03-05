86682429d8927591163b2a4a32475f3dc17a02b0
Xiretza <xiretza@gmail.com>
2018-03-05 17:03:28 +0100

    clean up wochenplan.py

--- wochenplan/wochenplan.py
+++ wochenplan/wochenplan.py
@@ -12,26 +12,6 @@ allergens = 'ABCDEFGHLMNOPR'
 
 days = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag']
 
-# data
-#   start_date:
-#   end_date:
-#   menus
-#     1
-#       price:
-#       days
-#         Montag
-#           items
-#             0
-#               name: 
-#               allergens:
-#             1
-#               name:
-#               allergens:
-
-data = {
-        'menus': {}
-}
-
 class Meal:
     def __init__(self, name, alls=None):
         self.name = name
@@ -45,19 +25,39 @@ class Meal:
     def __str__(self):
         return '%s (%s)' % (self.name, self.allergens)
 
+    def __repr__(self):
+        return '%s(%r, %r)' % (self.__name__, self.name, self.allergens)
+
 class Menu:
-    def __init__(self, name):
+    def __init__(self, name, price):
         self.name = name
+        if not isinstance(price, int):
+            raise ValueError('Price must be int: %r' % price)
+        self.price = price
         self.days = {}
 
-    def add_day(day, meals:"list of meals"):
+    def __str__(self):
+        pretty = 'Menu %s, %.2f€:' % (self.name, self.price / 100)
+        for day in days:
+            pretty += '\n%s:' % day
+            for meal in self.get_day(day):
+                pretty += '\n- %s' % meal
+
+        return pretty
+
+    def add_day(self, day, meals:"list of meals"):
         if not day in days:
             raise ValueError('Unknown day: %r' % day)
+        if day in self.days:
+            raise ValueError('Day already set: %r' % day)
         for meal in meals:
             if not isinstance(meal, Meal):
                 raise ValueError('Not a meal: %r' % meal)
 
-        self.days['day'] = meals
+        self.days[day] = meals
+
+    def get_day(self, day):
+        return self.days[day]
 
 
 def parse_fail(txt):
@@ -76,24 +76,26 @@ if m:
     end = m.group(2)
     year = m.group(3)
 
-    data['start_date'] = datetime.strptime('%s%s' % (start, year), '%d.%m.%Y').date()
-    data['end_date'] = datetime.strptime('%s%s' % (end, year), '%d.%m.%Y').date()
+    start_date = datetime.strptime('%s%s' % (start, year), '%d.%m.%Y').date()
+    end_date = datetime.strptime('%s%s' % (end, year), '%d.%m.%Y').date()
 else:
     parse_fail('document header: %r' % header)
 
 tab = root.find('.//table')
 
+menus = []
+
 for rownum, row in enumerate(tab.iter('tr')):
+    rowtype = rownum % 3
+
     for colnum, col in enumerate(row.iter('td')):
         # get all text inside field
         text = ''.join(col.itertext())
-        text = text.translate(str.maketrans('\n', ' ', '\t'))
-        
-        rowtype = rownum % 3
+        text = text.translate(str.maketrans('\n', ' ', '\t')).strip()
 
         # menu header
         if rowtype == 0:
-            m = re.search('^Menü (\d+) um € (\d+,\d{2})$', text.strip())
+            m = re.search('^Menü (\d+) um € (\d+,\d{2})$', text)
             if m:
                 menu_number = m.group(1)
                 if int(menu_number) != rownum / 3 + 1:
@@ -104,20 +106,13 @@ for rownum, row in enumerate(tab.iter('tr')):
                 if price % 10 != 0:
                     parse_fail('weird price: %d' % price)
 
-                data['menus'][menu_number] = {
-                        'price': price,
-                        'days': {}
-                    }
-                        
+                current_menu = Menu(menu_number, price)
+                menus.append(current_menu)
             else:
                 parse_fail('menu header: %r' % text)
         # days
         elif rowtype == 1:
-            if text.strip() == days[colnum]:
-                data['menus'][menu_number]['days'][days[colnum]] = {
-                        'items': []
-                    }
-            else:
+            if not text == days[colnum]:
                 parse_fail('wrong day: is %r, should be %r' % (text, days[colnum]))
         # food items
         elif rowtype == 2:
@@ -125,73 +120,50 @@ for rownum, row in enumerate(tab.iter('tr')):
             if len(items) != 2:
                 parse_fail('not two menus: %r' % items)
 
+            meals = []
             for num, item in enumerate(items):
+                # group 1: name of meal, group 2: allergens
                 item_regex = re.compile('^(.*?) +\(([%s]+)\)$' % allergens)
                 m = re.search(item_regex, item)
                 if m:
-                    data['menus'][menu_number]['days'][days[colnum]]['items'].append({
-                            'name': re.sub(' +', ' ', m.group(1)),
-                            'allergens': m.group(2)
-                        })
+                    meals.append(Meal(
+                        re.sub(' +', ' ', m.group(1)),
+                        m.group(2)
+                    ))
                 else:
                     parse_fail('menu item: %r' % item)
 
+            current_menu.add_day(days[colnum], meals)
+
 for day in days:
     soups = [
-        menu['days'][day]['items'][0]['name']
-        for menu in data['menus'].values()
-        ]
+        menu.get_day(day)[0].name
+        for menu in menus
+    ]
 
     if not len(set(soups)) == 1:
-        parse_fail('soups not equal on %s: %s' % (day, soups))
+        parse_fail('soups not equal on %s: %r' % (day, soups))
 
-class DateEncoder(json.JSONEncoder):
+class CustomEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, date):
             return obj.isoformat()
+        elif isinstance(obj, Menu):
+            return {'__kind': 'Menu', 'name': obj.name, 'price': obj.price, 'days': obj.days}
+        elif isinstance(obj, Meal):
+            return {'__kind': 'Meal', 'name': obj.name, 'allergens': obj.allergens}
 
-        return json.JSONEncoder.default(self, obj)
+        return super().default(obj)
 
-#for day in days:
-#    soup = data['menus']['1']['days'][days[0]]['items'][0]
-#
-#    meals = [[num, data['menus'][menu]['days'][day]['items'][1]] for (num, menu) in data['menus'].items()]
-#
-#    outstr = '%s\nSuppe: %s (%s)\n' % (day, soup['name'], soup['allergens'])
-#
-#    for (menu, meal) in meals:
-#        outstr += 'Menü %d: %s (%s)\n' % (menu, meal['name'], meal['allergens'])
-#
-#    print(outstr)
+for day in days:
+    soup = menus[0].get_day(day)[0]
 
-print(json.dumps(data, cls=DateEncoder, indent=2))
+    outstr = '%s\nSuppe: %s\n' % (day, soup)
 
+    for menu in menus:
+        meal = menu.get_day(day)[1]
+        outstr += 'Menü %s (%.2f€): %s\n' % (menu.name, menu.price / 100, meal)
 
+    print(outstr)
 
-#def strip_element(el):
-#    log('el %s, ' % el)
-#    if el.tag in stripped_tags:
-#        log('stripping, ')
-#        if len(el) == 0:
-#            text = ''.join(el.itertext())
-#            text = text.translate(str.maketrans('\t', ' ', '\r')) if len(text) > 0 else None
-#            log('results in %s\n' % repr(text))
-#        else:
-#            log('has children\n')
-#            return (strip_element(child) for child in list(el))
-#    else:
-#        log('not stripping, ')
-#        children = (strip_element(child) for child in list(el))
-#    
-#        el.clear()
-#
-#        for child in children:
-#            if isinstance(child, str):
-#                log('text += %s, ' % repr(child))
-#                el.text = (el.text if el.text else '') + child
-#            elif isinstance(child, ET.Element):
-#                log('appending %s, ' % child)
-#                el.append(child)
-#        
-#        log('\n')
-#        return el
+#print(json.dumps(menus, cls=CustomEncoder, indent=2))
