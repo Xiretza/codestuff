6a4d0fc3ee54bae413a44f6b47fcb8f25e4c2dc5
Xiretza <xiretza@gmail.com>
2018-04-22 18:08:53 +0200

    [untis] improve output formatting

--- untis/untis.py
+++ untis/untis.py
@@ -2,9 +2,10 @@ import requests
 import base64
 import json
 import argparse
+from tabulate import tabulate
 from collections import defaultdict
 from enum import Enum, unique
-from datetime import date, datetime, timezone
+from datetime import date, datetime, timedelta
 
 @unique
 class ElementType(Enum):
@@ -65,6 +66,10 @@ class Period():
                 datetime.strptime(str(data['endTime']), '%H%M').time()
             )
 
+        if self.end - self.start not in [timedelta(minutes=m) for m in [45, 50]]:
+            raise ValueError('Expected period to be 45 or 50 minutes, is %s (from %s to %s)' % (
+                self.end-self.start, self.start.strftime('%H:%M'), self.end.strftime('%H:%M')))
+
         self.state = PeriodState(data['cellState'])
         self.student_group = data['studentGroup']
 
@@ -85,24 +90,44 @@ class Period():
     def __str__(self):
         return '%s %s-%s: %s with %s in %s' % (
                 self.start.strftime('%d.%m.'), self.start.strftime('%H:%M'), self.end.strftime('%H:%M'),
-                ', '.join([e['longName'] for e in self.getElements(ElementType.subject)]),
-                ', '.join([e['longName'] for e in self.getElements(ElementType.teacher)]),
-                ', '.join([e['longName'] for e in self.getElements(ElementType.room)]),
+                ', '.join([e['name'] for e in self.getElements(ElementType.subject)]),
+                ', '.join([e['name'] for e in self.getElements(ElementType.teacher)]),
+                ', '.join([e['name'] for e in self.getElements(ElementType.room)]),
             )
 
 class Timetable():
     def __init__(self):
         self._days = defaultdict(list)
 
+    def periods(self):
+        """Returns a timedate-sorted list of all periods in the timetable"""
+        out = []
+        for day in self._days.values():
+            out += day
+
+        return sorted(out, key=lambda p: p.start)
+
     def add_period(self, period):
+        # check for overlap with existing periods
+        for other_p in self.periods():
+            if period.end <= other_p.start or other_p.end <= period.start:
+                # out of range or barely touching one side, both ok
+                continue
+
+            if (period.start == other_p.start) and (period.end == other_p.end):
+                # exactly the same, also ok
+                continue
+
+            raise ValueError('Periods overlap weirdly: %s and %s' % (other_p, period))
+
         day = self._days[period.start.date()]
 
         day.append(period)
-        day.sort(key=lambda l: l.start)
+        day.sort(key=lambda p: p.start)
 
     def days(self):
         """Return a sorted list of days"""
-        return (self._days[d] for d in sorted(self._days))
+        return [self._days[d] for d in sorted(self._days)]
 
 def fetch_config(schoolname, element_type, date):
     url = 'https://neilo.webuntis.com/WebUntis/api/public/timetable/weekly/pageconfig?type=%d&date=%s' % (element_type.value, date.strftime('%Y-%m-%d'))
@@ -148,9 +173,27 @@ def main():
     for period in data['data']['elementPeriods'][str(grade_id)]:
         tt.add_period(Period(period, elements))
 
+    # dict of rows, first index is start time, second index is date, third index is irrelevant (for multiple periods per time slot)
+    rows = {}
     for day in tt.days():
-        for period in day:
-            print(period)
+        for p in day:
+            if p.start.time() not in rows:
+                rows[p.start.time()] = {}
+
+            row = rows[p.start.time()]
+
+            if p.start.date() not in row:
+                row[p.start.date()] = []
+
+            row[p.start.date()].append(p)
+
+    indices = []
+    rows_mangled = []
+    for time in sorted(rows):
+        indices.append(time.strftime('%H:%M'))
+        rows_mangled.append({day.strftime('%A'): '\n'.join([list(p.getElements(ElementType.subject))[0]['name'] for p in periods]) for day, periods in rows[time].items()})
+
+    print(tabulate(rows_mangled, headers='keys', showindex=indices, tablefmt='grid'))
 
 if __name__ == '__main__':
     main()
